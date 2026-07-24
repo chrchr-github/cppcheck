91832cceee40fa5a5904a1a5155d35124a7f973d
chrchr-github <noreply@github.com>
2026-07-24 20:00:01 +0200

    Check op1

--- lib/valueflow.cpp
+++ lib/valueflow.cpp
@@ -3606,7 +3606,8 @@ static void valueFlowSymbolic(const TokenList& tokenlist, const SymbolDatabase& 
                 continue;
             if (tok->astOperand2()->exprId() == 0)
                 continue;
-            if (tok->astOperand2()->variable() && tok->astOperand2()->variable()->isArray() && tok->astOperand2()->variable()->getTypeName() != "std::array") // array to pointer decay
+            if (tok->astOperand2()->variable() && tok->astOperand2()->variable()->isArray() &&
+                tok->astOperand1()->valueType() && tok->astOperand1()->valueType()->pointer) // array to pointer decay
                 continue;
             if (!isConstExpression(tok->astOperand2(), settings.library))
                 continue;
